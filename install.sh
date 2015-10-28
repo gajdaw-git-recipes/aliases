14bedd804f95a1ef1d8405f940d10da90c613696
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-10-28 16:30:56 +0100

    Fix: empty-commit and empty-init aliases

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ git config --global alias.create-file "!createFile() { for name in \"\$@\"; do e
 git config --global alias.simple-commit "!simpleCommit() { for name in \"\$@\"; do git create-file \"\$name\"; git snapshot \$name; done; }; simpleCommit"
 git config --global alias.simple-loop "!simpleLoop() { NAME=\$1; i="1"; while [ \$i -le \$2 ]; do git simple-commit \$NAME\$i; i=\$[\$i+1]; done; }; simpleLoop"
 git config --global alias.multi-commit "!multiCommit() { for name in \"\$@\"; do git create-file $name; done; git snapshot \"\$*\"; }; multiCommit"
-git config --global alias.empty-commit "!emptyCommit() { git commit --allow-empty -m \"Empty commit\" }; emptyCommit"
-git config --global alias.empty-init "!emptyInit() { git init ; git commit --allow-empty -m \"Initial empty commit\" }; emptyInit"
+git config --global alias.empty-commit "!emptyCommit() { git commit --allow-empty -m \"Empty commit\"; }; emptyCommit"
+git config --global alias.empty-init "!emptyInit() { git init ; git commit --allow-empty -m \"Initial empty commit\"; }; emptyInit"
 
 
 
